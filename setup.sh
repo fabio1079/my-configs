0ab510905a5214f152df3966908c16f669a9c4fe
Fabio Teixeira <fabio1079@gmail.com>
2016-04-05 16:19:36 +0000

    Add a setup bash file
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+mv .gitconfig ~
+mv .vimrc ~
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim -c "PluginInstall"
+
+mkdir ~/.vim/colors
+cp ~/.vim/bundle/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+
